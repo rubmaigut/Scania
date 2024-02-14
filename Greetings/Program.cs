f11f4d4fa462c49de5dc4cb6e774e3792231f142
Maidelin R <maidelin.rubio@appliedtechnology.se>
2024-02-14 16:50:43 +0100

    Add: console app project

---
+++ Greetings/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello Scania Team, what is your Name?");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Nice to meet you, I'm Maidelin Rubio. Thank you, {name}, for this fun technical test. Looking forward to talking soon. Happy Coding!");
+    }
+}
